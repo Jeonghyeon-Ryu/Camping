c1f5d0b99b64837f75f76b871d63e221fd1548cf
admin <admin@YD2-22>
2022-10-18 12:35:16 +0900

    1011235

---
+++ Vue_Camping/src/components/note/CTextArea.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="type=='textBox'">
+    <textarea class="write_place" v-on:keyup.shift="shiftfUp($event)" v-on:keydown.shift="shiftfDown($event)"
+      v-on:keydown.enter="creTextarea($event)" :value="data">
+    </textarea>
+  </div>
+</template>
+<script>
+export default {
+  props: ['data'],
+  methods: {
+    shiftfUp: function (e) {
+      this.shiftSatus = false;
+    },
+    shiftfDown: function (e) {
+      this.shiftSatus = true;
+    },
+    creTextarea: function (e) {
+      if (!this.shiftSatus) {
+        e.preventDefault();
+
+        this.$emit('creArea');
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+.textbox {
+    background-color: beige;
+    border: 3px solid beige;
+    line-height: 10px;
+}
+.write_place {
+    display: inline-block;
+    height: fit-content;
+    width: 90%; 
+    border : none;
+}
+</style>
---
+++ Vue_Camping/src/components/note/CreateLine.vue
@@ -0,0 +1,18 @@
+<template>
+  <CTextArea :data="data"></CTextArea>
+</template>
+<script>
+import CTextArea from './CTextArea.vue';
+
+export default {
+  props:['type','data'],
+  created() {
+    console.log('type : ', this.type)
+    console.log('data : ', this.data)
+  }, 
+  components: { CTextArea }
+}
+</script>
+<style scoped>
+
+</style>
