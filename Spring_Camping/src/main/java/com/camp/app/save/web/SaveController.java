6e12e20558b4c8e45896050d12313cd734914be6
johaaaaa <jhy7651@naver.com>
2022-10-19 12:53:52 +0900

    22.10.19 12:53

--- Spring_Camping/src/main/java/com/camp/app/save/web/SaveController.java
+++ Spring_Camping/src/main/java/com/camp/app/save/web/SaveController.java
@@ -3,36 +3,42 @@ package com.camp.app.save.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.camp.app.save.service.SaveService;
 import com.camp.app.save.service.SaveVO;
 
-@Controller
-@CrossOrigin
+@RestController
+@CrossOrigin(originPatterns = "*")
 public class SaveController {
-	
-	@Autowired
-	SaveService service;
-	
-	public List<SaveVO> showAll(){	// 모든 저장목록 불러오기
-		
-		return null;
-	}
-	
-	@GetMapping("/save")
-	public boolean isExist(SaveVO save) {	// 게시글 하나 들어갈때 저장된 목록인지 확인
-		System.out.println("save : " + save);
-		return true;
-	}
-	public boolean save(SaveVO save) {	// 저장하기
-		
-		return true;
-	}
-	public boolean remove(SaveVO save) {	// 저장목록에서 제거
-		
-		return true;
-	}
+   
+   @Autowired
+   SaveService service;
+   
+   @GetMapping("/saves")
+   public List<SaveVO> showAll(){   // 모든 저장목록 불러오기
+      
+      return service.showAll();
+   }
+   
+   @GetMapping("/save")
+   public boolean isExist(SaveVO save) {   // 게시글 하나 들어갈때 저장된 목록인지 확인
+      return service.isExist(save);
+   }
+   
+   @PostMapping("/save")
+   public boolean save(@RequestBody SaveVO save) {   // 저장하기
+      System.out.println("save : " + save);
+      return service.save(save);
+   }
+   @DeleteMapping("/save")
+   public boolean remove(@RequestBody SaveVO save) {   // 저장목록에서 제거
+      
+      return service.remove(save);
+   }
 }
--- Vue_Camping/src/components/UsedPractice/UsedMain.vue
+++ Vue_Camping/src/components/UsedPractice/UsedMain.vue
@@ -49,36 +49,48 @@
             <li>
               <label for="inputPlace">지역</label>
                 <select v-model="regionSelect" id="districtSelect" name='city' @change="districtChange">
-                  <option value diabled>시/도</option> 
+                  <option value='' disabled>시/도</option> 
                   <option value='전체'>전체</option>
-                  <option value='서울특별시'>서울특별시</option>
-                  <option value='부산광역시'>부산광역시</option>
-                  <option value='대구광역시'>대구광역시</option>
-                  <option value='인천광역시'>인천광역시</option>
-                  <option value='광주광역시'>광주광역시</option>
-                  <option value='대전광역시'>대전광역시</option>
-                  <option value='울산광역시'>울산광역시</option>
-                  <option value='경기도'>경기도</option>
-                  <option value='강원도'>강원도</option>
-                  <option value='충청북도'>충청북도</option>
-                  <option value='충청남도'>충청남도</option>
-                  <option value='전라북도'>전라북도</option>
-                  <option value='전라남도'>전라남도</option>
-                  <option value='경상북도'>경상북도</option>
-                  <option value='경상남도'>경상남도</option>
-                  <option value='제주도'>제주도</option>
+                  <option value='서울'>서울특별시</option>
+                  <option value='부산'>부산광역시</option>
+                  <option value='대구'>대구광역시</option>
+                  <option value='인천'>인천광역시</option>
+                  <option value='광주'>광주광역시</option>
+                  <option value='대전'>대전광역시</option>
+                  <option value='울산'>울산광역시</option>
+                  <option value='경기'>경기도</option>
+                  <option value='강원'>강원도</option>
+                  <option value='충북'>충청북도</option>
+                  <option value='충남'>충청남도</option>
+                  <option value='전북'>전라북도</option>
+                  <option value='전남'>전라남도</option>
+                  <option value='경북'>경상북도</option>
+                  <option value='경남'>경상남도</option>
+                  <option value='제주특별자치도'>제주도</option>
                 </select>
                 <select v-model="regionSelect2" name='county' id="citySelect">
                   <option value disabled>시/군/구</option>
                   <option value='전체'>전체</option>
                 </select>
             </li>
-            <li>                          
-              <label for="inputPrice">가격범위</label>
-              <input v-model="minPrice" type="range" id="inputPrice" placeholder="0" name="minPrice" min="1000" max="10000000" step="1000">
+            <li>
+            <label for="inputPrice">가격범위</label>
+
+             <VueSimpleRangeSlider
+              style="width: 200px;
+                     line-height: 20px;
+                     font-size: 12px;"
+
+              :min="0"
+              :maxPrice="1000000"
+              exponential
+              v-model="state.range"
+            >
+              <template #prefix="{ value }">￦ </template>
+              </VueSimpleRangeSlider>
+              <!-- <input v-model="minPrice" type="range" id="inputPrice" placeholder="0" name="minPrice" min="1000" max="10000000" step="1000">
               <p>~</p>
-              <input v-model="maxPrice" type="range" id="inputPrice" placeholder="0" name="maxPrice" min="1000" max="10000000" step="1000">
-              <!-- <div id="slider"></div> -->
+              <input v-model="maxPrice" type="range" id="inputPrice" placeholder="0" name="maxPrice" min="1000" max="10000000" step="1000"> -->
             </li>
           </ul>
         </div>
@@ -120,9 +132,15 @@
   import district from "@/assets/district.js"
   import Swal from 'sweetalert2';
 
+  // 슬라이더
+  import VueSimpleRangeSlider from "vue-simple-range-slider";
+  import "vue-simple-range-slider/css";
+  import { reactive, defineComponent } from "vue";
+
   export default{
     components:{
     UsedCard,
+    VueSimpleRangeSlider
     },
     data(){
       return{
@@ -182,7 +200,7 @@
         let sido = document.querySelector('#districtSelect');
         let sigu = document.querySelector('#citySelect');
         let sidoName = sido.value;
-        let cityArr = ["서울특별시","부산광역시","인천광역시","대구광역시","광주광역시","대전광역시","울산광역시","경기도","강원도","충청북도","충청남도","경상북도","경상남도","전라북도","전라남도","제주도"];
+        let cityArr = ["서울","부산","인천","대구","광주","대전","울산","경기","강원","충북","충남","경북","경남","전북","전남","제주특별자치도"];
 
         sigu.options.length=1;  //저장내역 삭제
 
@@ -227,7 +245,11 @@
                     this.usedList = data;
                 }).catch(err=>console.log(err))
 
-    }
+    },
+    setup() {
+    const state = reactive({ range: [20, 1000], number: 10 });
+    return { state };
+  },
   }
 
 </script>
