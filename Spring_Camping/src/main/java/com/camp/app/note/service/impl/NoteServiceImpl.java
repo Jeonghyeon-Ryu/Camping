8c5ce359a669111d5c2b0b184a29d2b7da6226e2
yshyukk <01092103780@naver.com>
2022-10-20 18:22:51 +0900

    2022-10-20

--- Spring_Camping/src/main/java/com/camp/app/note/service/impl/NoteServiceImpl.java
+++ Spring_Camping/src/main/java/com/camp/app/note/service/impl/NoteServiceImpl.java
@@ -1,13 +1,20 @@
 package com.camp.app.note.service.impl;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.camp.app.note.mapper.NoteMapper;
 import com.camp.app.note.service.NoteDto;
+import com.camp.app.note.service.NoteImgVO;
 import com.camp.app.note.service.NoteService;
 import com.camp.app.note.service.NoteVO;
 
@@ -31,7 +38,46 @@ public class NoteServiceImpl implements NoteService{
 		nvo.setNoteId(mapper.getMaxNoteId());
 		return mapper.writeCotents(nvo);	
 	}
-	
+	//이미지저장
+	@Override
+	public boolean insertImg(List<MultipartFile> files) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		Date date = new Date();
+		String directoryPath = sdf.format(date);
+		String uploadPath = "D:\\upload\\note\\"+directoryPath;
+		
+		//폴더 주소
+		File uploadPathDir = new File(uploadPath);
+		if(!uploadPathDir.exists()) {
+			uploadPathDir.mkdirs();
+		}
+		files.forEach(file->{
+			NoteImgVO img = new NoteImgVO();
+			img.setOriginName(file.getOriginalFilename());
+			img.setImgFormat(img.getOriginName().substring(img.getOriginName().indexOf('.')+1));
+			img.setImgSize(file.getSize());
+			img.setImgPath(directoryPath);
+			//img.setRecruId(recruMapper.getMaxRecruId());
+			//uuid를 통한 랜덤으로 아이디 주기
+			UUID uuid = UUID.randomUUID();
+			String[] uuids = uuid.toString().split("-");
+			img.setStoredName(uuids[0]+"_"+img.getOriginName());
+			//업로드
+			File uploadFile = new File(uploadPath, img.getStoredName());
+			
+			try {
+				file.transferTo(uploadFile);
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			mapper.insertNoteImg(img);
+		});
+		return true;
+		
+	}
+
 	//MynoteList
 	@Override
 	public List<NoteVO> getMyNoteList(String email) {
