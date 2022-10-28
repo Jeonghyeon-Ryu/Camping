960989534a82afbb60977ddf322869a4320e961b
Jeonghyeon-Ryu <vxxv123@naver.com>
2022-10-28 11:55:53 +0900

    22.10.28 경로

--- Spring_Camping/src/main/java/com/camp/app/report/web/ReportController.java
+++ Spring_Camping/src/main/java/com/camp/app/report/web/ReportController.java
@@ -3,13 +3,13 @@ package com.camp.app.report.web;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.camp.app.report.service.ReportService;
@@ -17,6 +17,7 @@ import com.camp.app.report.service.ReportVO;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/java")
 public class ReportController {
 	
 	@Autowired
