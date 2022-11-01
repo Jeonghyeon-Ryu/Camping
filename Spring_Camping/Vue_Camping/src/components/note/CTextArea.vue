87163f0cdbf70980ade442223028dfe949adb331
띠용 <103794752+Jeonghyeon-Ryu@users.noreply.github.com>
2022-11-01 10:21:14 +0900

    Revert "Sh"

--- Spring_Camping/Vue_Camping/src/components/note/CTextArea.vue
+++ Spring_Camping/Vue_Camping/src/components/note/CTextArea.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="write_fn">
     <div class="left_container">
-      <div class="btn_container" @mouseover="show = true" @mouseleave="show = false">
-        <div class="drag_btn" ><img src="@/assets/img/note/drag.png"></div>
+      <div class="btn_container">
+        <div class="drag_btn"><img src="@/assets/img/note/drag.png"> </div>
         <div class="del_line"><img src="@/assets/img/note/trash.png" @click="delLine($event)"></div>
       </div>
     </div>
     <div class="textarea_container">
         <textarea class="write_place" v-on:keyup.shift="shiftfUp($event)" v-on:keydown.shift="shiftfDown($event)"
-        v-on:keydown.enter="creTextarea($event)" ></textarea>
+        v-on:keydown.enter="creTextarea($event)" :value="data"  @input="resize"></textarea>
     </div>
   </div>
 </template>
@@ -18,8 +18,7 @@ export default {
   props: ['data'],
   data() {
     return {
-      shiftSatus: false,
-      show : false
+      shiftSatus: false
     }
   },
   
@@ -38,18 +37,11 @@ export default {
         this.$emit('creArea');
       }
     },
-    /*resize() {
-      let element = this.$refs["textarea"];
-
-      element.style.height = "18px";
-      element.style.height = element.scrollHeight + "px";
-    },*/
     
   },
 }
 </script>
 
 <style scoped src="@/assets/css/note/CTextArea.css">
-/*@import url('https://fonts.googleapis.com/css2?family=Inconsolata&display=swap');*/
 
 </style>
